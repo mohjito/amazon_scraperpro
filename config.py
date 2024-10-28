d767c89cba5984f067d355581de6b2621e2d71d1
mohjito <121104038+mohjito@users.noreply.github.com>
2024-10-28 07:20:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from datetime import time
 BASE_URL = "https://www.amazon.in/s?k=two+brothers+organic+farms&page={}"
 MAX_PAGES = 2  # Change as needed
 SCRAPE_TIMES = [
-    time(hour=07, minute=23),
+    time(hour=7, minute=23),
     time(hour=7, minute=28),  # Add more entries for testing
     time(hour=6, minute=8),
     # Remove these after testing
